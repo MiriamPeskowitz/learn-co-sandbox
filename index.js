c613d56570b3139f5fe7a4203b413a075a3ebfdc
Miriam Peskowitz <miriam5000@gmail.com>
2021-06-11 13:52:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+class User {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+new User('bob')
+class Item {
+  constructor(name, price) {
+    this.name = name
+    this.price = price
+  }
+}
+new Item(400)
